358589e86d22d8b8e892edcbc33b124c1f81accd
Francois Budin <francois.budin@kitware.com>
2019-05-14 10:55:13 -0400

    WIP: Build shared libs Python

--- CMake/ITKModuleMacros.cmake
+++ CMake/ITKModuleMacros.cmake
@@ -291,20 +291,6 @@ macro(itk_module_impl)
     COMPONENT Development
     )
   itk_module_doxygen(${itk-module})   # module name
-  # Add custom install rules for third party libraries when building ITK with shared libraries
-  # to install these libraries inside the correct python directory. This will duplicate the
-  # third party libraries that are installed as we are not changing the original install rules
-  # provided by their respective package.
-  if(${PROJECT_NAME}_THIRD_PARTY AND ITK_WRAP_PYTHON)
-   file(GLOB_RECURSE list_directories LIST_DIRECTORIES true *)
-    foreach(file_name ${list_directories})
-      if(IS_DIRECTORY ${file_name} AND EXISTS ${file_name}/CMakeLists.txt)
-        get_property(THIRD_PARTY_TARGETS DIRECTORY ${file_name} PROPERTY BUILDSYSTEM_TARGETS)
-        message(WARNING "Third party targets: (${file_name}) ${THIRD_PARTY_TARGETS}")
-      endif()
-    endforeach()
-  endif()
-
 endmacro()
 
 # itk_module_link_dependencies()
